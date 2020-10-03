797bdf6350e88e9ee3cd2f4cd8d1c8248ca9dbd5
Manish <manishjobs1999@gmail.com>
2020-10-03 22:21:25 +0530

    [Feat]Add Array Question

---
+++ Array/Contain_Function_String.java
@@ -0,0 +1,10 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Contain_Function_String {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("atach".contains("a"));
+    }
+}
