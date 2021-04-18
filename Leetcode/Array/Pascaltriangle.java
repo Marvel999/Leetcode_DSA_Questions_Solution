d0e8670fb2c69870948a7562d3f92cc027fffe68
flipxonsoft <manishjobs1999@gmail.com>
2021-04-19 01:07:12 +0530

    [Add] Add question in Leetcode Array Package

--- Leetcode/Array/Pascaltriangle.java
+++ Leetcode/Array/Pascaltriangle.java
@@ -1,2 +1,22 @@
-package Leetcode.Array;public class Pascaltriangle {
+package Leetcode.Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pascaltriangle {
+
+        public List<List<Integer>> generate(int numRows) {
+            List<List<Integer>> out=new ArrayList<>();
+            for(int  i=0;i<numRows;i++){
+                List<Integer> templist=new ArrayList<>();
+                int ncr=1;
+                for(int  j=0;j<=i;j++){
+                    ncr=ncr*(i-j)/(j+1);;
+                    templist.add(ncr);
+                }
+                out.add(templist);
+            }
+            return out;
+        }
+
 }
