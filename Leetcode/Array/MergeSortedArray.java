e8e1ef4f746786b957df8a6cce721f167911b724
Mervel999 <manishjobs1999@gmail.com>
2023-04-02 21:42:18 +0530

    Marge matrix without extra space

---
+++ Leetcode/Array/MergeSortedArray.java
@@ -0,0 +1,12 @@
+package Leetcode.Array;
+
+import java.util.Arrays;
+
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = m; i<(m+n); i++){
+            nums1[i]= nums2[i-m];
+        }
+        Arrays.sort(nums1);
+    }
+}
